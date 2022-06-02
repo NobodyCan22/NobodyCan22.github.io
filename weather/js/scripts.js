54d4a9045428fdf377880c9776d02ffa6134dace
NobodyCan22 <minglingreality@gmail.com>
2022-06-01 22:24:08 -0600

    All the Preston Town Page starties

---
+++ weather/js/scripts.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+
+x.onclick = toggleMenu;
+
+document.getElementById("date").innerHTML = new Date(document.Date)
