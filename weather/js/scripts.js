08f40c014baf6c1d465c8b3010d9bef1a84784c7
NobodyCan22 <minglingreality@gmail.com>
2022-06-03 22:42:42 -0600

    finished Preston Town Page

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -1,10 +1,28 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
 const x = document.getElementById("hamburgerBtn");
-
 x.onclick = toggleMenu;
 
 document.getElementById("date").innerHTML = new Date(document.Date)
+
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=42.09699837220583&lon=-111.87595677773281&units=imperial&appid=c50b2a933706495fab439a581827e04e';
+fetch(url)
+    .then((response) => {
+        if(!response.ok) console.log(response.status + ' : ' + response.statusText);
+        response.json()
+            .then((data) => {
+                document.getElementById("currentTemp").innerHTML = data.current.temp;
+                document.getElementById("maxTemp").innerHTML = data.daily[0].temp.max;
+                document.getElementById("minTemp").innerHTML = data.daily[0].temp.min;
+                document.getElementById("windSpeed").innerHTML = data.daily[0].wind_speed;
+            });
+    });
+
+
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}    
+
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
